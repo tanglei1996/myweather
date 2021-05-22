88acd4b59d8c0982e2c33243821ba45e229dae4c
tanglei1996 <1336631096@qq.com>
2021-05-22 16:13:03 +0800

    加入显示天气功能

--- app/src/main/java/com/myweather/android/util/Utility.java
+++ app/src/main/java/com/myweather/android/util/Utility.java
@@ -2,9 +2,11 @@ package com.myweather.android.util;
 
 import android.text.TextUtils;
 
+import com.google.gson.Gson;
 import com.myweather.android.db.City;
 import com.myweather.android.db.County;
 import com.myweather.android.db.Province;
+import com.myweather.android.gson.Weather;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -77,4 +79,20 @@ public class Utility {
         }
         return false;
     }
+
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
